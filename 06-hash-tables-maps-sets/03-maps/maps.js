b8d80d45f000869857196dd45da066ae92764338
Manjeet Verma <mnjit.vrma@gmail.com>
2026-06-02 00:21:49 +0200

    refactor: reorganize map operations for clarity and consistency

--- 06-hash-tables-maps-sets/03-maps/maps.js
+++ 06-hash-tables-maps-sets/03-maps/maps.js
@@ -9,3 +9,20 @@ console.log(nameMap.values());
 //get keys
 console.log(nameMap.keys());
 
+
+nameMap.forEach((i, index) => {
+    console.log(i, index);
+})
+
+//Check
+console.log(nameMap.has(2));
+
+// Get value
+
+console.log(nameMap.get(1));
+
+// Delete
+
+console.log(nameMap.delete(1));
+
+console.log(nameMap);
